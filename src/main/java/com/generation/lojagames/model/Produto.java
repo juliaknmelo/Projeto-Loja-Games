969e35996c57d4a4187d85a3ac01b0247bb998dd
juliaknmelo <ju-kar98@hotmail.com>
2024-05-27 18:13:48 -0300

    primeiro commit

---
+++ src/main/java/com/generation/lojagames/model/Produto.java
@@ -0,0 +1,77 @@
+package com.generation.lojagames.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name="tb_produto")
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "O atributo nome é obrigatório!")
+	@Size(min = 4, max = 100, message = "O atributo nome deve conter no mínimo 04 e no máximo 100 caracteres")
+	private String nome;
+
+	@NotNull(message = "O atributo valor é obrigatório!")
+	private float valor;
+	
+	@NotNull(message = "O atributo estoque é obrigatório!")
+	private Integer estoque;
+	
+	@ManyToOne 
+	@JsonIgnoreProperties("produto") 
+	private Categoria categoria;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public float getValor() {
+		return valor;
+	}
+
+	public void setValor(float valor) {
+		this.valor = valor;
+	}
+
+	public Integer getEstoque() {
+		return estoque;
+	}
+
+	public void setEstoque(Integer estoque) {
+		this.estoque = estoque;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+	
+}
